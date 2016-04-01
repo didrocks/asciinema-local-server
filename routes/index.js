865a190a7ed4e33a5229c447e8557c99b9b35bc8
Didier Roche <didrocks@ubuntu.com>
2016-04-01 10:07:17 +0200

    Redirect asciicinema direct requests to index.html

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
+var debug = require('debug')('asciinema-local-server:router');
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+/*router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});*/
+
+/* return home page for asciinema file request */
+router.get(/.*\.asciinema$/, function (req, res, next) {
+  debug('asciinema file requested: ' + req);
+  res.sendFile(path.join(__dirname, '..', 'www', 'index.html'));
 });
 
 module.exports = router;
